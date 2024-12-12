e3c5e47c4e25003d7d2a3b3f76fa444b3ef59001
Jeong <cogmlwjd7@naver.com>
2024-12-12 13:49:39 +0900

    commit test2

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data.js";
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcepts(props) {
-  return <li>
-    <img src={props.image} alt={props.title} />
-    <h3>{props.title}</h3>
-    <p>{props.description}</p>
-  </li>
-}
+import Header from "./components/Header.jsx";
+import CoreConcepts from "./components/CoreConcept.jsx";
 
 function App() {
   return (
@@ -39,7 +12,7 @@ function App() {
         <ul><CoreConcepts {...CORE_CONCEPTS[0]}/>
           <CoreConcepts {...CORE_CONCEPTS[1]}/>
           <CoreConcepts {...CORE_CONCEPTS[2]}/>
-          <CoreConcepts {...CORE_CONCEPTS[3]}/> 
+          <CoreConcepts {...CORE_CONCEPTS[3]}/>
         </ul>
         </section>
       </main>
